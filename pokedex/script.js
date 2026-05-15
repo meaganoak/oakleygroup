fcef93449d430f790a2d2808e145c71b0ca6c296
Meagan Oakley <meagan@Meagans-MacBook-Pro.local>
2026-05-15 15:30:55 +0100

    pokedex

---
+++ pokedex/script.js
@@ -0,0 +1,52 @@
+const team = [
+  {
+    name: "MEAGAN",
+    role: "Head of Oak's Laboratory",
+    bio: "Obsessed with computers",
+    image: "images/meagan.jpg"
+  }, 
+  {
+    name: "LANA",
+    role: "Gym leader",
+    bio: "",
+    image: "images/lana.jpg"
+  }
+]
+
+let current = 0
+
+function renderMember() {
+  document.getElementById("member-name").textContent =
+    team[current].name
+
+  document.getElementById("member-role").textContent =
+    team[current].role
+
+  document.getElementById("member-bio").textContent =
+    team[current].bio
+
+  document.getElementById("member-image").src =
+    team[current].image
+}
+
+document.getElementById("next").onclick = () => {
+  current = (current + 1) % team.length
+  renderMember()
+}
+
+document.getElementById("prev").onclick = () => {
+  current = (current - 1 + team.length) % team.length
+  renderMember()
+}
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight") {
+    current = (current + 1) % team.length
+    renderMember()
+  }
+
+  if (e.key === "ArrowLeft") {
+    current = (current - 1 + team.length) % team.length
+    renderMember()
+  }
+})
